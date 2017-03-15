41142c9cb93c11f5b8a648d9a0d50e103ab03909
rahultimbadiya <timbadiyarahul@gmail.com>
2017-03-15 14:49:23 -0700

    Add files via upload

---
+++ cgi-bin/insert.py
@@ -0,0 +1,34 @@
+import cgi, cgitb
+import mysql.connector
+
+cgitb.enable()
+
+#this function will authenticate server and then connect with the database
+db = mysql.connector.connect(host="localhost", user="root", password="root@123", database="student")
+cursor = db.cursor()
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+ID = int(form.getvalue('ID'))
+Name = form.getvalue('Name')
+
+#This query insert the data intothe database with given values
+sql = "INSERT INTO student_data(student_id,student_name) VALUES ('%d','%s')"%(ID, Name)
+cursor.execute(sql) # this execute the query
+
+
+db.commit() #commit all changes
+db.close()  #close the connection from database
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print("<h4> ID=%d and Name=%s are inserted</h4>")%(ID,Name)
+print ("</body>")
+print ("</html>")
+
